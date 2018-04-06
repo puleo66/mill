fdce4f061b33b7285155006e2094a5074c288b4d
puleo66 <puleo@freemail.hu>
2018-04-06 13:34:19 +0200

    Init project

---
+++ app/javascript/components/Hello.js
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Hello extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div>
+          <h1>Hello React!</h1>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Hello
